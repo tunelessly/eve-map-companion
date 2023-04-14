8641f41a4460ce3aacc6f7ce131f650094754e92
tunelessly <9685607+tunelessly@users.noreply.github.com>
2023-04-14 01:47:00 +0100

    Works

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './app.css'
-import App from './App.svelte'
+//import App from './App.svelte'
+import App from './lib/mambo.svelte';
 
 const app = new App({
   target: document.getElementById('app'),
-})
+});
 
 export default app
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import liveReload from 'vite-plugin-live-reload';
+import ViteRestart from 'vite-plugin-restart';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    liveReload(["./**/*.ts", "./**/*.svelte"], { root: process.cwd() }),
+    ViteRestart({
+      restart: ["./**/*.ts", "./**/*.svelte"]
+    })
+  ],
+  ssr: {
+    noExternal: ["three", "troika-three-text"]
+  }
+});
