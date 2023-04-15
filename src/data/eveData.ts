0f5e3dc8e7e9cc695ab6d7879e07c585596335eb
tunelessly <9685607+tunelessly@users.noreply.github.com>
2023-04-15 19:05:12 +0100

    Stuff

--- src/data/eveData.ts
+++ src/data/eveData.ts
@@ -1,151 +1,14 @@
 import eveUniverse from './universe-pretty.json';
+import { Galaxy } from './universe';
 import { HSV2RGB, RGBtofloat, sectoHSV, coordinatestoGeometry, type coordinates3D } from '../utils/geometry';
-import { forceSimulation, forceManyBody } from "d3-force";
 import { Color } from 'three';
-
-class Galaxy {
-    private readonly _name: String = "New Eden";
-    private readonly _regions: Record<string, Region>;
-
-    constructor(regions = {}) {
-        this._regions = regions;
-    }
-
-    public addRegion(region: Region): void {
-        this._regions[region.name] = region;
-    }
-
-    public getRegion(regionID: string): Region | undefined {
-        return this._regions[regionID];
-    }
-
-    get regions() {
-        return this._regions;
-    }
-
-}
-
-class Region {
-    private readonly _id: string;
-    private readonly _name: string;
-    private readonly _systems: Record<number, System>;
-
-    constructor(id, name, systems = {}) {
-        this._id = id;
-        this._name = name;
-        this._systems = systems;
-    }
-
-    get id() {
-        return this._id;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get systems() {
-        return this._systems;
-    }
-
-    public addSystem(system: System): void {
-        this._systems[system.id] = system;
-    }
-
-    public getSystem(systemID: number): System | undefined {
-        return this._systems[systemID];
-    }
-}
-
-class System {
-    public readonly coordinates: coordinates3D;
-    public readonly securityStatus: number;
-    public readonly name: string;
-    public readonly id: number;
-    public readonly region: string;
-    private readonly _links: number[]
-
-    constructor(coordinates, securityStatus, region, id, name, links = []) {
-        this.coordinates = coordinates;
-        this.securityStatus = securityStatus;
-        this.region = region;
-        this.id = id;
-        this.name = name;
-        this._links = links;
-    }
-
-    public addLink(systemID): void {
-        this._links.push[systemID];
-    }
-
-    get links() {
-        return this._links;
-    }
-}
-
-const createGraph = () => {
-    const galaxy = new Galaxy();
-
-    for (let regionID in eveUniverse.regions) {
-        let regionName = eveUniverse.regions[regionID];
-        let region = new Region(regionID, regionName);
-        galaxy.addRegion(region)
-    }
-
-    for (let s of eveUniverse.solarSystems) {
-        let system = new System(
-            { x: s.x, y: s.y, z: s.z },
-            s.security,
-            s.region,
-            s.id,
-            s.name
-        );
-        galaxy.getRegion(system.region).addSystem(system);
-    }
-
-    for (let jump of eveUniverse.jumps) {
-        let candidate: System;
-        for (let regionID in galaxy.regions) {
-            let system = galaxy.getRegion(regionID).getSystem(jump.from);
-            if (system === undefined) {
-                continue;
-            }
-            else {
-                candidate = system;
-                break;
-            }
-        }
-        candidate?.addLink(jump.to);
-    }
-
-    Object.keys(galaxy.regions).forEach(regionName => {
-        console.log(`Region: ${regionName}`);
-        Object.keys(galaxy.getRegion(regionName).systems).forEach(systemName => {
-            console.log(`\t${systemName}`);
-        });
-    });
-
-}
-
-const forceGrapgh = (coordinates: coordinates3D[]): coordinates3D[] => {
-    const simulation = forceSimulation(coordinates)
-    .force("charge", forceManyBody().strength(-1))
-    .stop();
-
-    simulation.tick(300);
-
-    // this simulation is not 3d aware and thus we must remap the
-    // z and y axes to one another so that they'll display correctly 
-    // on a 3d environment
-
-    return coordinates.map(coordinate => {
-        return {x: coordinate.x, y: coordinate.z, z: coordinate.y}
-    });
-}
-
 
 export const generateGeometryData = () => {
     // createGraph();
+    const galaxy: Galaxy = new Galaxy();
+    galaxy.populateGalaxy(eveUniverse);
+    const regionCoordinates = galaxy.produceCoordinates("Aridia");
+
     const coordinates: coordinates3D[] = [];
     const colors: number[] = [];
     const color = new Color();
@@ -161,9 +24,9 @@ export const generateGeometryData = () => {
         colors.push(color.r, color.g, color.b);
     }
 
-    const newCoordinates = forceGrapgh(coordinates);
-    const positions = coordinatestoGeometry(newCoordinates);
-    // const positions = coordinatestoGeometry(coordinates);
+    // const newCoordinates = forceGrapgh(coordinates);
+    // const positions = coordinatestoGeometry(newCoordinates);
+    const positions = coordinatestoGeometry(coordinates);
 
     return { positions, colors };
 };
---
+++ src/data/universe.ts
@@ -0,0 +1,217 @@
+import type { coordinates3D } from '../utils/geometry';
+import { forceSimulation, forceManyBody } from "d3-force";
+import { ok, err, Result } from "neverthrow";
+
+type SolarSystem = {
+    name: string
+    id: number
+    security: number
+    region: string
+    x: number
+    y: number
+    z: number
+}
+
+type Jumps = {
+    from: number
+    to: number
+}
+
+export type EvEUniverse = {
+    regions: Record<string, string>
+    solarSystems: SolarSystem[]
+    jumps: Jumps[]
+}
+
+class System {
+    public readonly coordinates: coordinates3D;
+    public readonly securityStatus: number;
+    public readonly name: string;
+    public readonly id: number;
+    public readonly region: string;
+    private readonly _links: number[]
+
+    constructor(coordinates, securityStatus, region, id, name, links = []) {
+        this.coordinates = coordinates;
+        this.securityStatus = securityStatus;
+        this.region = region;
+        this.id = id;
+        this.name = name;
+        this._links = links;
+    }
+
+    public addLink(systemID): void {
+        this._links.push[systemID];
+    }
+
+    get links() {
+        return this._links;
+    }
+}
+
+class Region {
+    private readonly _id: string;
+    private readonly _name: string;
+    private readonly _systems: Record<number, System> = {};
+    private readonly _remappedSystems: Record<number, System> = {};
+
+    constructor(id, name) {
+        this._id = id;
+        this._name = name;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get systems() {
+        return this._systems;
+    }
+
+    public addSystem(system: System): void {
+        this._systems[system.id] = system;
+    }
+
+    public addRemmapedSystem(system: System): void {
+        this._remappedSystems[system.id] = system;
+    }
+
+    public getSystem(systemID: number): Result<System, string> {
+        const system = this._systems[systemID];
+        if (system === undefined) {
+            return err(`System ID not found: ${systemID}`);
+        }
+        else {
+            return ok(system)
+        }
+    }
+}
+
+export class Galaxy {
+    private readonly _name: String = "New Eden";
+    private readonly _regions: Record<string, Region> = {};
+
+    public addRegion(region: Region): void {
+        this._regions[region.name] = region;
+    }
+
+    public getRegion(regionID: string): Result<Region, string> {
+        const region = this._regions[regionID];
+        if (region === undefined) {
+            return err(`Region not found: ${regionID}`);
+        }
+        else {
+            return ok(region);
+        }
+    }
+
+    get regions() {
+        return this._regions;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    public populateGalaxy = (sourceData: EvEUniverse) => {
+
+        for (let regionID in sourceData.regions) {
+            let regionName = sourceData.regions[regionID];
+            let region = new Region(regionID, regionName);
+            this.addRegion(region)
+        }
+
+        for (let s of sourceData.solarSystems) {
+            let system = new System(
+                { x: s.x, y: s.y, z: s.z },
+                s.security,
+                s.region,
+                s.id,
+                s.name
+            );
+            this.getRegion(system.region).map(region => {
+                region.addSystem(system);
+            }).mapErr(console.log);
+        }
+
+        for (let jump of sourceData.jumps) {
+            for (let regionID in this.regions) {
+                this.getRegion(regionID).andThen(region => {
+                    return region.getSystem(jump.from);
+                }).map(system => {
+                    system.addLink(jump.to);
+                }).mapErr(console.log);
+            }
+        }
+
+        // Object.keys(this.regions).forEach(regionName => {
+        //     console.log(`Region: ${regionName}`);
+        //     Object.keys(this.getRegion(regionName).systems).forEach(systemName => {
+        //         console.log(`\t${systemName}`);
+        //     });
+        // });
+
+    }
+
+    private produceSystemThing<T>(regionName: string, thing: (arg: System) => T): T[] {
+        const resultList = this.getRegion(regionName).map(region => {
+            // For fuck's sake
+            return (Object.keys(region.systems) as unknown as Array<keyof typeof region.systems>).map(key => {
+                return region.getSystem(key).map(thing).mapErr(console.log);
+            });
+        });
+        return resultList.andThen(list => Result.combine(list)).mapErr(console.log).unwrapOr([]);
+    }
+
+    public produceCoordinates = (regionName: string): coordinates3D[] => {
+        return this.produceSystemThing(regionName, system => system.coordinates);
+    }
+
+    public produceSecurityStatuses = (regionName: string): number[] => {
+        return this.produceSystemThing(regionName, system => system.securityStatus);
+    }
+
+
+    public subway = (regionName): void => {
+        this.getRegion(regionName).map(region => {
+            let coordinates = Object.keys(region.systems).map(systemID => {
+                const system: System = region.systems[systemID];
+                return { systemID: system.id, ...system.coordinates };
+            });
+
+            const simulation = forceSimulation(coordinates)
+                .force("charge", forceManyBody().strength(-1))
+                .stop();
+
+
+            simulation.tick(300);
+
+            coordinates.map(coordinate => {
+                // remap the axes - we've lost the z axis and if we display this as is we'll get a disc
+                // viewed edge-on.
+                // Should this belong to this data structure?
+                // TODO
+                return { systemID: coordinate.systemID, x: coordinate.x, y: coordinate.z, z: coordinate.y }
+            })
+                .forEach(coordinate => {
+                    region.getSystem(coordinate.systemID).map(system => {
+                        // copy every stat from the original system
+                        // except the coordinates
+                        const coordinates = { x: coordinate.x, y: coordinate.y, z: coordinate.z };
+                        const securityStatus = system.securityStatus;
+                        const regionName = region.name;
+                        const systemID = system.id;
+                        const systemName = system.name;
+                        const links = system.links;
+                        const remappedSystem = new System(coordinates, securityStatus, regionName, systemID, systemName, links);
+                        region.addRemmapedSystem(remappedSystem);
+                        console.log(`Remapped system: ${remappedSystem.name} to ${remappedSystem.coordinates.x} ${remappedSystem.coordinates.y} ${remappedSystem.coordinates.z} `);
+                    });
+                });
+        }).mapErr(console.log);
+    }
+}
