6ee134b976686f78359be6880900bba1f67043c3
tunelessly <9685607+tunelessly@users.noreply.github.com>
2023-04-24 00:05:42 +0100

    Revert "Feature/offline map"

--- src/view/Canvas.svelte
+++ src/view/Canvas.svelte
@@ -11,7 +11,7 @@
 		getRegionNames,
 		generateGalaxy,
 		generateRegion,
-	} from "../controller/controller.js";
+	} from "../controller/controller";
 	import { writable } from "svelte/store";
 
 	let selectedRegion = "-";
