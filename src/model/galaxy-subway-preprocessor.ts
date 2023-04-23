be4991d04a4ef223b08788f14248403d7c16f354
tunelessly <9685607+tunelessly@users.noreply.github.com>
2023-04-23 02:18:45 +0100

    Removed cruft

--- src/model/galaxy-subway-preprocessor.ts
+++ src/model/galaxy-subway-preprocessor.ts
@@ -59,17 +59,6 @@ const offendingRegions = [
     "The Spire",
 ]
 
-const median = (n: number[]) => {
-    const sorted = Array.from(n).sort((a, b) => a - b);
-    const middle = Math.floor(sorted.length / 2);
-
-    if (sorted.length % 2 === 0) {
-        return (sorted[middle - 1] + sorted[middle]) / 2;
-    }
-
-    return sorted[middle];
-}
-
 export const regionPreProcess = (data: EvESubway): EvESubway => {
 
     offendingRegions.forEach(regionName => {
