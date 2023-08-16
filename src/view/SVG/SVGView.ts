9718181242cd65b73a4bb6a7694e34a9fb81bae9
tunelessly <9685607+tunelessly@users.noreply.github.com>
2023-08-16 20:07:49 +0100

    Potentially fixed the 'doesn't zoom' bug.

--- src/view/SVG/SVGView.ts
+++ src/view/SVG/SVGView.ts
@@ -205,10 +205,8 @@ export class SVGView implements ViewLike {
         const selectedNode: SVGTextElement = <SVGTextElement>selection.node();
         const x = selectedNode.x.baseVal[0].value;
         const y = selectedNode.y.baseVal[0].value;
-        this.zoom.translateTo(this.SVG, x, y);
-        // TODO: Why...?
-        this.zoom.translateTo(this.SVG, x, y);
         this.zoom.scaleTo(this.SVG, 3);
+        this.zoom.translateTo(this.SVG, x, y);
     }
 
 
